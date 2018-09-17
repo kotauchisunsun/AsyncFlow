5fb9e5df12768c466b1bebf17ad872b646176a52
Ryo Kurauchi <kotauchisunshine@hotmail.co.jp>
2018-09-18 01:07:18 +0900

    asyncflow.testのlint調整

--- test/asyncflow.test.ts
+++ test/asyncflow.test.ts
@@ -1,19 +1,19 @@
-import {ISubscriber} from '../src/ISubscriber'
-import {SimpleSubscriber}  from '../src/SimpleSubscriber'
-import {SimplePublisher} from '../src/SimplePublisher'
-import {Transformer} from '../src/Transformer'
-import {IfPipe} from '../src/IfPipe'
+import {IfPipe} from '../src/IfPipe';
+import {ISubscriber} from '../src/ISubscriber';
+import {SimplePublisher} from '../src/SimplePublisher';
+import {SimpleSubscriber} from '../src/SimpleSubscriber';
+import {Transformer} from '../src/Transformer';
 
 describe ('asyncflowのテスト', () => {
-    it( 'ループ処理で1,2,3,4を生成する', () => {
-        jest.useFakeTimers()
+    it('ループ処理で1,2,3,4を生成する', () => {
+        jest.useFakeTimers();
         expect.assertions(4);
-        const pub = new SimplePublisher<number,ISubscriber<number>>(1);
-        const pipe = new IfPipe<number,ISubscriber<number>>( (x:number) => x < 5 );
-        const sub = new SimpleSubscriber<number,void>( (x:number) => {
-            expect([1,2,3,4].includes(x)).toBeTruthy()
+        const pub = new SimplePublisher<number, ISubscriber<number>>(1);
+        const pipe = new IfPipe<number, ISubscriber<number>>((x: number) => x < 5);
+        const sub = new SimpleSubscriber<number, void>((x: number) => {
+            expect([1, 2, 3, 4].includes(x)).toBeTruthy();
         });
-        const trans = new Transformer<number,number,ISubscriber<number>>( (x:number) => x + 1);
+        const trans = new Transformer<number, number, ISubscriber<number>>((x: number) => x + 1);
         pipe.subscribe(pub.content);
         trans.subscribe(pipe.trueContent);
         sub.subscribe(pipe.trueContent);
