d7cf062d08dbf90ff8049fd9e7d97dbd60c49dde
Ryo Kurauchi <kotauchisunshine@hotmail.co.jp>
2018-09-18 00:55:42 +0900

    primeのlint調整

--- src/prime.ts
+++ src/prime.ts
@@ -1,30 +1,30 @@
 import {IfPipe} from './IfPipe';
 import {SimplePublisher} from './simple_publisher';
-import {SimpleSubscriber}  from './simple_subscriber';
+import {SimpleSubscriber} from './simple_subscriber';
 import {Transformer} from './transformer';
 
 const pub = new SimplePublisher(1);
-const relay = new Transformer( (x:number) => x); 
-const inc = new Transformer( (x:number) => x+1);
+const relay = new Transformer((x: number) => x);
+const inc = new Transformer((x: number) => x + 1);
 
 relay.subscribe(pub.content);
 inc.subscribe(relay.content);
 relay.subscribe(inc.content);
 
-const init = new Transformer( (x:number) => [x,x]);
+const init = new Transformer((x: number) => [x, x]);
 init.subscribe(relay.content);
 
-const dec = new Transformer( (x:number[]) => [x[0]-1,x[1]]);
+const dec = new Transformer((x: number[]) => [x[0] - 1, x[1]]);
 dec.subscribe(init.content);
 
-const check = new IfPipe( (x:number[]) => (x[0]>1 && x[1] % x[0] != 0));
+const check = new IfPipe((x: number[]) => (x[0] > 1 && x[1] % x[0] !== 0));
 check.subscribe(dec.content);
-dec.subscribe(check.trueContent)
+dec.subscribe(check.trueContent);
 
-const is_prime = new IfPipe( (x:number[]) => (x[0]==1 && x[1]!=1))
-is_prime.subscribe(check.falseContent);
+const isPrime = new IfPipe((x: number[]) => (x[0] === 1 && x[1] !== 1));
+isPrime.subscribe(check.falseContent);
 
-const sub = new SimpleSubscriber( (x:number[]) => console.log(x[1]));
-sub.subscribe(is_prime.trueContent);
+const sub = new SimpleSubscriber((x: number[]) => {console.log(x[1]); });
+sub.subscribe(isPrime.trueContent);
 
 pub.publish();
