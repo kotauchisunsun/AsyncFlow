0839b08f74f0af07b0dfff0a74a0198baa552a09
Ryo Kurauchi <kotauchisunshine@hotmail.co.jp>
2018-09-18 00:34:37 +0900

    Bloadcasterのlint調整

--- src/Bloadcaster.ts
+++ src/Bloadcaster.ts
@@ -2,22 +2,22 @@ import {EventEmitter} from 'events';
 
 export class Bloadcaster<V, S> {
     private emitter: EventEmitter;
-    private event_name: string;
+    private eventName: string;
     private f: (s: S, v: V) => void;
 
     constructor(f: (s: S, v: V) => void) {
         this.emitter = new EventEmitter();
-        this.event_name = 'event_name';
+        this.eventName = 'event_name';
         this.f = f;
     }
 
     public register(sub: S) : void {
-        this.emitter.on(this.event_name, (v: V) => this.f(sub, v));
+        this.emitter.on(this.eventName, (v: V) => {this.f(sub, v); });
     }
 
     public cast(v: V) : void {
         setImmediate(() =>
-            this.emitter.emit(this.event_name, v)
+            this.emitter.emit(this.eventName, v)
         );
     }
 }
